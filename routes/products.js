a665506d6099fbfd4435fd129038a8e06072b771
Numan <nomanmani62@gmail.com>
2021-10-10 15:34:28 +0500

    dummy routes and controllers working

---
+++ routes/products.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { getAllProducts } = require('../controllers/products');
+
+router.get('/', getAllProducts);
+
+module.exports = router;
